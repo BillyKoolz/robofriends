81d700c2999130ccf1b02b0371f6b969c66b19eb
BillyKoolz <89686968+BillyKoolz@users.noreply.github.com>
2022-11-28 16:57:20 +0100

    Implemented redux; Changes thru lecture 389; TSL

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,56 +1,65 @@
-import React, {useState, useEffect} from 'react'; 
+import React, { Component} from 'react'; 
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import './App.css';
 
+import {setSearchField} from '../actions';
 
-function App() {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         robots: [],
-    //         searchfield: '',
-    //     }
-    // }
-    
-    const [robots, setRobots] = useState([]);                           //  React Hooks (Lecture 377)
-    const [searchfield, setSearchfield] = useState('');                 //  React Hooks (Lecture 377)
-    const [count, setCount] = useState(0);
-    
-    // componentDidMount() {
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //         .then(response => response.json())
-    //         .then(users=> this.setState({robots: users}))
-    // }
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField,            //  Would be searchField: state.searchRobots.searchField, if we had multiple reducers
+    }
+}
 
-    useEffect(() => {                                                   //  React Hooks (Lecture 377)
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users=> setRobots(users))
-    }, [count]);                                                             //  Optional second argument with empty list to avoid infinite loop; 
-                                                                        //  useEffect now mimics ComponentDidMount
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            //searchfield: '',                                              //  Commented out when implementing Redux (lecture 389) 
+        }
     }
     
-    //const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-    })
-    return !robots.length ?
-        <h1>Loading</h1> :
-    (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <button onClick={() => setCount(count+1)} >Click Me!</button>
-            <SearchBox searchChange={onSearchChange}/>
-            <Scroll>
-                <CardList robots={filteredRobots}/>
-            </Scroll>
-        </div>
-    );
+    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users=> this.setState({robots: users}))
+    }
+                                                                        
+    
+    // const onSearchChange = (event) => {                                  //  Commented out when implementing Redux (lecture 389) 
+    //     this.setState({ searchfield: event.target.value})
+    // }
+    
+    render() {
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        return !robots.length ?
+            <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+    
     
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
+//import {createStore} from 'redux'; DEPRECATED
+import { configureStore } from '@reduxjs/toolkit';
+import {searchRobots} from './reducers';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+const store = configureStore({
+  reducer: searchRobots,
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
